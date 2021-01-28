88bdba834c40aed55236cc091a3021a9e3f30576
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2021-01-27 18:51:47 -0800

    Created Xcode Project

---
+++ sqyr/sqyr/sqyrApp.swift
@@ -0,0 +1,17 @@
+//
+//  sqyrApp.swift
+//  sqyr
+//
+//  Created by David Barsamian on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct sqyrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
