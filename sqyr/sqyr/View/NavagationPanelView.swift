d4b0b0db1633ec7ae4cce1e9b0b915e2009365c1
Steven Phun <phun.steven@gmail.com>
2021-02-10 03:23:16 -0800

    nav panel, pop-up mockup completed

---
+++ sqyr/sqyr/View/NavagationPanelView.swift
@@ -0,0 +1,70 @@
+//
+//  NavagationPanelView.swift
+//  sqyr
+//
+//  Created by Steven Phun on 2/10/21.
+//
+
+import SwiftUI
+
+struct NavagationPanelView: View {
+    let buildings = getBuildings()
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                SearchBarView(text: .constant(""))
+                
+                HStack {
+                    Spacer()
+                    HStack {
+                        Circle()
+                            .fill(Color.gray)
+                            .opacity(0.2)
+                            .frame(width: 20)
+                        Text("Academic")
+                    }
+                    Spacer()
+                    HStack {
+                        Circle()
+                            .fill(Color.gray)
+                            .opacity(0.2)
+                            .frame(width: 20)
+                        Text("Social")
+                    }
+                    Spacer()
+                }
+                .frame(height: 20)
+                .padding()
+                
+                List(buildings, id: \.self) { building in
+                    NavigationLink(destination: BuildingPopUpView(building: building)) {
+                        HStack {
+                            Rectangle()
+                                .frame(width: 20, height: 20)
+                                .foregroundColor(.black)
+                            Text(building)
+                        }
+                    }
+                }
+                .navigationTitle("CBU Buildings")
+            }
+        }
+    }
+}
+
+func getBuildings() -> Array<String> {
+    var arrayOfBuildings = [String]()
+    
+    for i in 1...10 {
+        arrayOfBuildings.append("Building #\(i)")
+    }
+    
+    return arrayOfBuildings
+}
+
+struct NavagationPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavagationPanelView()
+    }
+}
---
+++ sqyr/sqyr/View/SearchBarView.swift
@@ -0,0 +1,66 @@
+//
+//  SearchBarView.swift
+//  sqyr
+//
+//  Created by Steven Phun on 2/10/21.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    @Binding var text: String
+     
+    @State private var isEditing = false
+ 
+    var body: some View {
+        HStack {
+            TextField("Search for a place", text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                 
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+ 
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+ 
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+
+        SearchBarView(text: .constant(""))
+    }
+}
