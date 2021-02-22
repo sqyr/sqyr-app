b18e0a802149d9ae7ede04cb796785fbf398f6c6
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2021-02-22 14:59:36 -0800

    Base AR View Work
    - Created ARCLViewController, which handles using the ARCL library in UIKit terms
    - Created ARMapView, which wraps ARCLViewController in a SwiftUI view
    - Created LandmarkCalloutView, which is the base view for Landmark nametags. This currently is not implemented due to technical constraints.

--- sqyr/sqyr/View/ARCLViewController.swift
+++ sqyr/sqyr/View/ARCLViewController.swift
@@ -9,6 +9,7 @@ import ARKit_CoreLocation
 import CoreLocation
 import Foundation
 import UIKit
+import SwiftUI
 
 class ARCLViewController: UIViewController {
     var sceneLocationView = SceneLocationView()
@@ -25,17 +26,24 @@ class ARCLViewController: UIViewController {
         
         sceneLocationView.frame = view.bounds
         
-        // Apple Park
         var nodes: [LocationAnnotationNode] = []
-        nodes.append(buildNode(lat: 37.3326, long: -122.0055, altitude: 200, image: UIImage(systemName: "mappin")!))
+        
+        let hostingVC = UIHostingController(rootView: LandmarkCalloutView(title: "Apple Park", distance: "!Distance"))
+        hostingVC.loadView()
+        
+        // Apple Park
+        nodes.append(buildNode(lat: 37.3326, long: -122.0055, altitude: 200, view: hostingVC.view))
         
         // CBU
-        nodes.append(buildNode(lat: 33.9289, long: -117.4259, altitude: 200, image: UIImage(systemName: "mappin")!))
+        nodes.append(buildNode(lat: 33.9289, long: -117.4259, altitude: 200, image: UIImage(systemName: "graduationcap.fill")!.withTintColor(.blue).withRenderingMode(.alwaysTemplate)))
+        
+        // Ten Ren's Tea Time
+        nodes.append(buildNode(lat: 33.9758, long: -117.5751, altitude: 200, image: UIImage(systemName: "cube.fill")!.withTintColor(.green).withRenderingMode(.alwaysTemplate)))
         
         sceneLocationView.addLocationNodesWithConfirmedLocation(locationNodes: nodes)
     }
     
-    /// Returns a new LocationAnnotationNode with the inputted parameters
+    /// Returns a new LocationAnnotationNode with an image
     /// - Parameter lat: The latitude of the location
     /// - Parameter long: The longitude of the location
     /// - Parameter altitude: The altitude of the location
@@ -44,5 +52,16 @@ class ARCLViewController: UIViewController {
         let coord = CLLocationCoordinate2D(latitude: lat, longitude: long)
         let location = CLLocation(coordinate: coord, altitude: altitude)
         return LocationAnnotationNode(location: location, image: image)
+    }
+    
+    /// Returns a new LocationAnnotationNode with a view
+    /// - Parameter lat: The latitude of the location
+    /// - Parameter long: The longitude of the location
+    /// - Parameter altitude: The altitude of the location
+    /// - Parameter view: The view to dipslay for the node
+    private func buildNode(lat: CLLocationDegrees, long: CLLocationDegrees, altitude: CLLocationDistance, view: UIView) -> LocationAnnotationNode {
+        let coord = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let location = CLLocation(coordinate: coord, altitude: altitude)
+        return LocationAnnotationNode(location: location, view: view)
     }
 }
