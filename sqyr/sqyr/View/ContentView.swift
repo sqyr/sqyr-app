7d384dd0be183da426c42e679f63f751d553a9f7
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2021-02-22 15:04:42 -0800

    Revert "Merge functional-ar-code -> main"
    This reverts commit 6f9a51b3bb0b2ea19e7f8bcad85b4bcb0344d932, reversing
    changes made to 80df4acb0d4f8f45695f21fc74b2ca802fa805dd.

--- sqyr/sqyr/View/ContentView.swift
+++ sqyr/sqyr/View/ContentView.swift
@@ -3,21 +3,16 @@
 //  sqyr
 //
 //  Created by David Barsamian, Lauren Nelson, Steven Phun, True Sarmiento, and Tomas Perez on 1/27/21.
- 
+
 import SwiftUI
 import CoreLocation
 import ARKit_CoreLocation
-import Drawer
 
 struct ContentView: View {
     var sceneLocationView = SceneLocationView()
     
     var body: some View {
-<<<<<<< HEAD
         return NavView().edgesIgnoringSafeArea(.all)
-=======
-        ARMapView().edgesIgnoringSafeArea(.all)
->>>>>>> functional-ar-code
     }
 }
 
