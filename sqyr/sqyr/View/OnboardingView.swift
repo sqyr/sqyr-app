942b8614527e8fc99857fd2400096f4522608d77
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2021-03-01 15:56:29 -0800

    Onboarding
    - More work on onboarding view

--- sqyr/sqyr/View/OnboardingView.swift
+++ sqyr/sqyr/View/OnboardingView.swift
@@ -14,9 +14,35 @@ struct OnboardingView: View {
             Text("Welcome to Sqyr")
                 .font(.largeTitle)
                 .bold()
+            Spacer().frame(height: 20)
+            VStack {
+                OnboardingDetail(systemName: "figure.walk", header: "Get Directions", desc: "Discover important places on campus, like classrooms.", color: .blue)
+                OnboardingDetail(systemName: "mappin.and.ellipse", header: "", desc: <#T##String#>, color: <#T##Color#>)
+            }
             Spacer()
-            HStack {
-                
+        }
+        .frame(width: UIScreen.main.bounds.width * 0.8)
+    }
+}
+
+private struct OnboardingDetail: View {
+    var systemName: String
+    var header: String
+    var desc: String
+    var color: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: systemName)
+                .renderingMode(.template)
+                .foregroundColor(color)
+                .font(.largeTitle)
+                .padding()
+            VStack(alignment: .leading) {
+                Text(header)
+                    .font(.headline)
+                Text(desc)
+                    .font(.body)
             }
         }
     }
