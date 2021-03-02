06b569f1da4b80ce3f054efe854dc1b89e8f3a35
Steven Phun <phun.steven@gmail.com>
2021-03-01 23:00:07 -0800

    change UIColor type from Color

--- sqyr/Constant.swift
+++ sqyr/Constant.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 // MARK: - COLOR
 
-let backgroundColor: Color = #colorLiteral(red: 0.02065248787, green: 0.1662423909, blue: 0.3424238265, alpha: 1)
-let accentColor: Color = #colorLiteral(red: 0.6361396313, green: 0.4641576409, blue: 0.1666345596, alpha: 1)
+let backgroundColor: UIColor = #colorLiteral(red: 0.02065248787, green: 0.1662423909, blue: 0.3424238265, alpha: 1)
+let accentColor: UIColor = #colorLiteral(red: 0.6361396313, green: 0.4641576409, blue: 0.1666345596, alpha: 1)
 
 
 
