e7454dbe7b92f2c07b1a6c1a0a3a49162c9dc602
Steven Phun <phun.steven@gmail.com>
2021-03-01 22:53:05 -0800

    add constant color

---
+++ sqyr/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  sqyr
+//
+//  Created by Steven Phun on 3/1/21.
+//
+
+import SwiftUI
+
+// MARK: - COLOR
+
+let backgroundColor: Color = #colorLiteral(red: 0.02065248787, green: 0.1662423909, blue: 0.3424238265, alpha: 1)
+let accentColor: Color = #colorLiteral(red: 0.6361396313, green: 0.4641576409, blue: 0.1666345596, alpha: 1)
+
+
+
+
