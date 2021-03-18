9e8b1500af68af41afd4cdc0b993873364e73cd1
tomasperez13 <63325595+tomasperez13@users.noreply.github.com>
2021-03-17 19:25:43 -0700

    created all necessary data models
    Created Landmark, Studyroom, Classroom, and User. Created HTTP Client folder with request template

---
+++ sqyr/sqyr/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  sqyr
+//
+//  Created by Tomas Perez on 3/17/21.
+//
+
+import Foundation
+
+struct User: Codable{
+    var id: Int?
+    var firstName: String
+    var studyRoomId: Studyroom
+    var creation: Date
+}
+    
