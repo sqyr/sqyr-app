cc75ffdbd9a29f20c9f2f74a46e7afc4bfed6889
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2021-03-18 17:59:14 -0700

    Removed Deprecations
    - Removed deprecations, including:
    1. Drawer package
    2. DrawerContent view
    - Reformatted HTTPLandmarkClient

--- sqyr/sqyr/HTTP Clients/HTTPLandmarkClient.swift
+++ sqyr/sqyr/HTTP Clients/HTTPLandmarkClient.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 // To call this in the code, setup as such:
 // HTTPLandmarkClient().saveLandmark(landMarkName: "", etc...){ success in
 //      if success{
@@ -16,24 +15,23 @@ import Foundation
 //          do something else or show error message
 //      }
 // }
-class HTTPLandmarkClient: ObservableObject{
+class HTTPLandmarkClient: ObservableObject {
     // This is just an example of a possible function.
     // List all other functions in here for data manipulation.
-    func saveLandmark(landMarkName: String, description: String, hours: String, coordinatesLat: Double, coordinatesLon: Double, buildingType: String, icon: String, completion: @escaping (Bool) -> Void){
-        
-        guard let url = URL(string: "http://localhost:8080/Landmarks") else{
+    static func saveLandmark(landMarkName: String, description: String, hours: String, coordinatesLat: Double, coordinatesLon: Double, buildingType: String, icon: String, completion: @escaping (Bool) -> Void) {
+        guard let url = URL(string: "http://localhost:8080/Landmarks") else {
             fatalError("URL is not defined")
         }
-        
+
         let landMark = Landmark(landMarkName: landMarkName, description: description, hours: hours, coordinatesLat: coordinatesLat, coordinatesLon: coordinatesLon, buildingType: buildingType, icon: icon)
-        
+
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = try? JSONEncoder().encode(landMark)
-        
-        URLSession.shared.dataTask(with: request){ data, response, error in
-            guard let _ = data, error == nil else{
+
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let _ = data, error == nil else {
                 return completion(false)
             }
             completion(true)
--- sqyr/sqyr/View/Navigation/NavigationDrawer.swift
+++ sqyr/sqyr/View/Navigation/NavigationDrawer.swift
@@ -6,7 +6,6 @@
 //
 
 import BottomSheet
-import Drawer
 import SwiftUI
 
 // MARK: - View
@@ -36,37 +35,5 @@ struct NavigationDrawer: View {
                 }
             })
             
-    }
-}
-
-@available(*, deprecated, message: "This will be removed in a future version. Use a bottomSheet view modifier instead.")
-private struct DrawerContent: View {
-    var geoProxy: GeometryProxy
-    @State var heights: [CGFloat]
-    @ObservedObject var globalModel: GlobalModel
-
-    var body: some View {
-        return Drawer(heights: $heights) {
-            ZStack {
-                // Background
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(Color(UIColor.systemBackground))
-                    .shadow(radius: 100)
-
-                // Content
-                NavigationPanelView(globalModel: globalModel)
-                    .padding(.vertical)
-
-                // Grey Pill Indicator
-                VStack(alignment: .center) {
-                    Spacer().frame(height: 4.0)
-                    RoundedRectangle(cornerRadius: 3.0)
-                        .foregroundColor(Color.gray.opacity(0.5))
-                        .frame(width: 40.0, height: 6.0)
-                    Spacer()
-                }
-            }
-        }
-        .impact(.medium)
     }
 }
