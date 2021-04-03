60a3e185af81eaa4323db492567d661c6d0f8c18
Steven Phun <phun.steven@gmail.com>
2021-04-03 15:58:59 -0700

    add json bundle extension

--- sqyr/sqyr/Supporting Files/Constant.swift
+++ sqyr/sqyr/Supporting Files/Constant.swift
@@ -13,4 +13,29 @@ func floorPlanImage(building: String, floor: Int) -> String {
     return "\(building)_Floor\(floor)"
 }
 
+// MARK: - EXTENSION
 
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        // 1. Locate the json file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        
+        // 2. Create a property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        // 3. Create a decoder
+        let decoder = JSONDecoder()
+        
+        // 4. Create a property for the decoded data
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        // 5. Return the ready-to-use data
+        return loaded
+    }
+}
