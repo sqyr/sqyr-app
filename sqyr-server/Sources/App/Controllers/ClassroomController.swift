a5eef1af0ad11d4134884d8c1367eac3934670f5
Tomas Perez <46548725+tomas13perez@users.noreply.github.com>
2021-04-11 01:56:07 -0700

    ClassroomController big boom

---
+++ sqyr-server/Sources/App/Controllers/ClassroomController.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tomas Perez on 4/10/21.
+//
+import Foundation
+import Vapor
+import Fluent
+
+final class ClassroomController {
+
+    func create(_ req: Request) throws -> EventLoopFuture<ClassRoom> {
+        let classroom = try req.content.decode(ClassRoom.self)
+        return classroom.create(on: req.db).map { classroom }
+    }
+
+    func all(_ req: Request) throws -> EventLoopFuture<[ClassRoom]>{
+        ClassRoom.query(on: req.db).all()
+    }
+
+    func byID(_ req: Request) throws -> EventLoopFuture<[ClassRoom]>{
+        guard let classroomId = req.parameters.get("RoomID", as: Int.self) else{
+            throw Abort(.notFound)
+        }
+        return ClassRoom.query(on: req.db).filter(\.$id, .equal, classroomId)
+            .all()
+    }
+
+    func getByLandmarkId(_ req: Request) throws -> EventLoopFuture<[ClassRoom]>{
+        guard let landmarkId = req.parameters.get("LandMarkID", as: Int.self) else{
+            throw Abort(.notFound)
+        }
+
+        return ClassRoom.query(on: req.db).filter(\.$landmark.$id, .equal, landmarkId)
+            .with(\.$landmark)
+            .all()
+    }
+}
