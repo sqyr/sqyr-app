cc3e8af3a54ad40492c5457fb76ec874cebaadc4
Steven Phun <phun.steven@gmail.com>
2021-04-12 01:30:15 -0700

    add create, join, studygroup search features

--- sqyr/sqyr/View/Landmarks/StudyGroupView.swift
+++ sqyr/sqyr/View/Landmarks/StudyGroupView.swift
@@ -14,9 +14,182 @@ struct StudyGroupView: View {
     
     @State var studyGroupSearchText: String = ""
     @State var studyRoomSearchText: String = ""
-    @State var disclosureGroupExpand: Bool = true
     @State var username: String = ""
+    @State var disclosureGroupExpand: Bool = true
+    
     @ObservedObject var globalModel: GlobalModel
+    
+    // MARK: FUNCTIONS
+    
+    // show alert window
+    func showAlertView(room: Int, group: String, create: Bool) {
+        let message = create ? "Make your own study group." : group
+        let alert = UIAlertController(title: "Room \(room)", message: message, preferredStyle: .alert)
+        
+        if create {
+            alert.addTextField { section in
+                section.placeholder = "Enter class: EGR302"
+            }
+        }
+        alert.addTextField { username in
+            username.placeholder = "Enter Your Name"
+        }
+        
+        let buttonAction = UIAlertAction(title: create ? "Create" : "Join", style: .default) { (_) in
+            if alert.textFields != nil {
+                if create {
+                    let groupName = alert.textFields![0].text!
+                    let userName = alert.textFields![1].text!
+                    updateStudyGroup(key: room, group: groupName, user: userName)
+                } else {
+                    let userName = alert.textFields![0].text!
+                    joinStudyGroup(room: room, groupName: group, user: userName)
+                }
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { (_) in }
+        
+        // add into view
+        alert.addAction(buttonAction)
+        alert.addAction(cancelAction)
+        
+        // present alert
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: {})
+    }
+    
+    // TODO start - replace codes server side code
+    @State var studyGroups: Dictionary<Int,[Dictionary<String,[String]>]> = Dictionary<Int,[Dictionary<String,[String]>]>()
+    
+    var initlizeStudyGroup: Dictionary<Int,[Dictionary<String,[String]>]> {
+        var dict: Dictionary<Int,[Dictionary<String,[String]>]> = Dictionary<Int,[Dictionary<String,[String]>]>()
+        for room in landmark.getLandmarkRoomNumbers {
+            dict[room] = [Dictionary<String,[String]>]()
+        }
+        
+        studyGroups = dict
+        return dict
+    }
+    
+    func getStudyGroupSize(roomNum: Int) -> Int {
+        if studyGroups[roomNum] != nil {
+            return studyGroups[roomNum]!.count
+        }
+        
+        return 0
+    }
+
+    func getStudyGroupName(roomNum: Int) -> [String] {
+        if studyGroups[roomNum] != nil {
+            var array: [String] = []
+            
+            for dict in studyGroups[roomNum]! {
+                for (key, _) in dict {
+                    array.append(key)
+                }
+            }
+            
+            return array
+        }
+        
+        return []
+    }
+    
+    func updateStudyGroup(key: Int, group: String, user: String) {
+        var dict: [Int: [[String: [String]]]] = studyGroups
+        let value: [String: [String]] = [group: [user]]
+        
+        if dict[key] == nil {
+            dict[key] = []
+        }
+        
+        dict[key]!.append(value)
+        studyGroups = dict
+    }
+    
+    func getUserGroupSize(room: Int, group: String) -> Int {
+        let array: [[String: [String]]] = studyGroups[room]!
+        
+        for dict in array {
+            for (key, _) in dict {
+                if key == group {
+                    return dict[key]!.count
+                }
+            }
+        }
+        
+        return 0
+    }
+    
+    func getUserName(room: Int, group: String, user: Int) -> String {
+        let array: [[String: [String]]] = studyGroups[room]!
+        
+        for dict in array {
+            for (key, _) in dict {
+                if key == group {
+                    return dict[key]![user]
+                }
+            }
+        }
+        
+        return ""
+    }
+    
+    func joinStudyGroup(room: Int, groupName: String, user: String) -> Void {
+        var temp = studyGroups
+        var count = -1
+    
+        for group in temp[room]! {
+            count = count + 1
+            for (key, _) in group {
+                if key == groupName {
+                    (((temp[room]!)[count])[key]!).append(user)
+                }
+            }
+        }
+        
+        studyGroups = temp
+    }
+    
+    func displayRoomFilter() -> [Int] {
+        let roomsAll: [Int] = landmark.getLandmarkRoomNumbers
+        var roomsFiltered: [Int] = []
+        
+        // display all rooms
+        if self.studyGroupSearchText.isEmpty && self.studyRoomSearchText.isEmpty {
+            return roomsAll
+        }
+        
+        // display only search rooms
+        if self.studyGroupSearchText.isEmpty {
+            for room in roomsAll {
+                if "\(room)".starts(with: self.studyRoomSearchText.lowercased()) {
+                    roomsFiltered.append(room)
+                }
+            }
+        }
+        
+        // display only search study groups
+        if self.studyRoomSearchText.isEmpty {
+            var set: Set<Int> = []
+            for room in roomsAll {
+                for group in getStudyGroupName(roomNum:room) {
+                    if group.contains(self.studyGroupSearchText.lowercased()) {
+                        set.insert(room)
+                    }
+                }
+            }
+            
+            for room in set {
+                roomsFiltered.append(room)
+            }
+        }
+
+        
+        return roomsFiltered
+    }
+    
+    // TODO end - replace codes server side code
     
     var body: some View {
         VStack {
@@ -33,16 +206,16 @@ struct StudyGroupView: View {
             
             // LIST OF ROOMS
             List {
-                ForEach((landmark.getLandmarkRoomNumbers).filter({ "\($0)".contains(self.studyRoomSearchText.lowercased()) || self.studyRoomSearchText.isEmpty }), id: \.self) { room in
+                ForEach(displayRoomFilter(), id: \.self) { room in
                     DisclosureGroup("Room \(room)") {
-                        ForEach((0..<2).filter({ "\($0)".contains(self.studyGroupSearchText.lowercased()) || self.studyGroupSearchText.isEmpty }), id: \.self) { group in
-                            DisclosureGroup("Study Group \(group)") {
-                                ForEach(0..<3) { user in
-                                    Text("Student \(user)")
+                        ForEach((getStudyGroupName(roomNum:room)), id: \.self) { group in
+                            DisclosureGroup("\(group)") {
+                                ForEach(0..<getUserGroupSize(room: room, group: group), id: \.self) { user in
+                                    Text(getUserName(room: room, group: group, user: user))
                                         .foregroundColor(.secondary)
                                         .font(.footnote)
                                 } //: LOOP - USER
-                                Button(action: { showAlertView(title: "Room \(room)", message: "Group \(group)", create: false) }) {
+                                Button(action: { showAlertView(room: room, group: group, create: false) }) {
                                     RoundedRectangle(cornerRadius: 12)
                                         .fill(Color("gold"))
                                         .padding(.horizontal)
@@ -53,7 +226,7 @@ struct StudyGroupView: View {
                             .foregroundColor(Color("blue"))
                         } //: LOOP - STUDY GROUP
                         DisclosureGroup("Create Your Own Group", isExpanded: .constant(true)) {
-                        Button(action: { showAlertView(title: "Room \(room)", message: "Make your own study group." , create: true) }) {
+                        Button(action: { showAlertView(room: room, group: "", create: true) }) {
                             RoundedRectangle(cornerRadius: 12)
                                 .fill(Color("blue"))
                                 .padding(.horizontal)
@@ -69,31 +242,6 @@ struct StudyGroupView: View {
             .listStyle(InsetGroupedListStyle())
         } //: VSTACK
         .edgesIgnoringSafeArea(.bottom)
-    }
-    
-    func showAlertView(title: String, message: String, create: Bool) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        if create {
-            alert.addTextField { section in
-                section.placeholder = "Enter class: EGR302"
-            }
-        }
-        alert.addTextField { username in
-            username.placeholder = "Enter Your Name"
-        }
-        
-        let buttonAction = UIAlertAction(title: create ? "Create" : "Join", style: .default) { (_) in
-            if alert.textFields != nil {
-                username = alert.textFields![0].text!
-            }
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { (_) in }
-        
-        alert.addAction(buttonAction)
-        alert.addAction(cancelAction)
-        
-        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: {})
     }
 }
 
@@ -113,7 +261,7 @@ struct StudyGroupSearchBar: View {
     let placeholder: String
     @State private var isSearching: Bool = false
     @Binding var text: String
-    
+
     var body: some View {
         VStack {
 
