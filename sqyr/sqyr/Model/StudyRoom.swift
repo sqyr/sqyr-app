900f13c0b208158e3aab7194c5f849b6b9555732
David Barsamian <DavidJ.Barsamian@calbaptist.edu>
2021-04-13 17:14:33 -0700

    Added initializers for in-app creation of models
    Signed-off-by: David Barsamian <DavidJ.Barsamian@calbaptist.edu>

--- sqyr/sqyr/Model/StudyRoom.swift
+++ sqyr/sqyr/Model/StudyRoom.swift
@@ -20,11 +20,18 @@ struct StudyRoom: Codable {
     var name: String?
     var usersInStudyRoom: [User]?
     
+    // JSON Decoding
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try? container.decodeIfPresent(Int.self, forKey: .id)
         classRoomId = try? container.decodeIfPresent(ClassRoom.self, forKey: .classRoomId)
         name = try? container.decodeIfPresent(String.self, forKey: .name)
         usersInStudyRoom = try? container.decodeIfPresent([User].self, forKey: .usersInStudyRoom)
+    }
+    
+    // Creation In-App
+    init(classRoomId: ClassRoom, name: String) {
+        self.classRoomId = classRoomId
+        self.name = name
     }
 }
--- sqyr/sqyr/Model/User.swift
+++ sqyr/sqyr/Model/User.swift
@@ -20,11 +20,18 @@ struct User: Codable {
     var studyRoomId: StudyRoom?
     var creation: Date?
     
+    // JSON Decoding
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try? container.decodeIfPresent(Int.self, forKey: .id)
         firstName = try? container.decodeIfPresent(String.self, forKey: .firstName)
         studyRoomId = try? container.decodeIfPresent(StudyRoom.self, forKey: .studyRoomId)
         creation = try? container.decodeIfPresent(Date.self, forKey: .creation)
+    }
+    
+    // Creation In-App
+    init(firstName: String, studyRoomId: StudyRoom?) {
+        self.firstName = firstName
+        self.studyRoomId = studyRoomId
     }
 }
